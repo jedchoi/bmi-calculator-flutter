8789755c27f5594f56774a5715e5e20c8aca6d23
JaeHyuk Choi <jaehyuk1220@gmail.com>
2022-09-02 15:03:09 +0900

    Extract Widget 으로 리팩토링

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -17,72 +17,51 @@ class _InputPageState extends State<InputPage> {
           Row(
             children: [
               Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  height: 200.0,
-                  width: 170.0,
-                ),
+                child: ReusableCard(),
               ),
               Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  height: 200.0,
-                  width: 170.0,
-                ),
+                child: ReusableCard(),
               ),
             ],
           ),
           Row(
             children: [
               Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  height: 200.0,
-                  width: 170.0,
-                ),
+                child: ReusableCard(),
               ),
             ],
           ),
           Row(
             children: [
               Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  height: 200.0,
-                  width: 170.0,
-                ),
+                child: ReusableCard(),
               ),
               Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  height: 200.0,
-                  width: 170.0,
-                ),
+                child: ReusableCard(),
               ),
             ],
           ),
         ],
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      height: 200.0,
+      width: 170.0,
     );
   }
 }
